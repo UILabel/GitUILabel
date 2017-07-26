1d4101e1e92ce611de4d9035e533c368badc8b6b
UILabel <WangSai@WangSaideiMac.local>
2017-07-26 15:50:48 +0800

    创建项目

---
+++ GitUILabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUILabel
+//
+//  Created by WangSai on 2017/7/26.
+//  Copyright © 2017年 WangSai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitUILabel/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  GitUILabel
+//
+//  Created by WangSai on 2017/7/26.
+//  Copyright © 2017年 WangSai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label =[[UILabel alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    label.text =@"shajk";
+    label.textColor =[UIColor redColor];
+    [self.view addSubview:label];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
